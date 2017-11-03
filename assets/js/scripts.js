eb6fefaa0c54be00f83a3b8fe3e3554e31a41e57
ifeanyidavid <davekalu46@gmail.com>
2017-11-03 08:39:06 +0100

    I see some changes!!!
    Made homepage navbar fixed and fixed some alignment issues on desktop and mobile.

--- assets/js/scripts.js
+++
@@ -1,8 +0,0 @@
-// Get a reference to the menu class
-// const menu = document.querySelector(".menu");
-// const menu_toggle = document.querySelector(".menu-toggle");
-
-// Toggle the menu-open class when the menu is clicked
-// menu_toggle.addEventListener('click',function(){
-//     menu.classList.toggle('menu-open');
-// }, true);
